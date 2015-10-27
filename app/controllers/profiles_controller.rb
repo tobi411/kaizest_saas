da3ce65556214e384e9ec29d7c5b39273ab1a0c6
Tobi Kehinde <tokehinde@hotmail.com>
2015-10-27 00:45:49 +0000

    added before filters to check user action

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,4 +1,6 @@
 class ProfilesController < ApplicationController #Controllers should always be pluralized
+    before_action :authenticate_user! #make sure the user is logged in
+    before_action :only_current_user
     def new
         @user = User.find(params[:user_id]) #find logged in user
         @profile = @profile = Profile.new #not using @user.build_profile(profile_params) to avoid unlinking user to profile
@@ -20,9 +22,24 @@ class ProfilesController < ApplicationController #Controllers should always be p
         @profile = @user.profile
     end
     
+    def update
+        @user = User.find(params[:user_id])
+        @profile = @user.profile
+        if @profile.update_attributes(profile_params) #saves any changes to the database
+            flash[:success] = "Profile Updated!"
+            redirect_to user_path(params[:user_id])
+        else
+            render action: :edit
+        end
+    end
+    
     private 
         def profile_params
             #whitelisting used to save data from forms to database and for safety
             params.require(:profile).permit(:first_name, :last_name, :job_title, :phone_number, :contact_email, :description) 
+        end
+        def only_current_user
+          @user = User.find( params[:user_id] )
+          redirect_to(root_url) unless @user == current_user #if the user passed through the url is the current user, proceed. If not go back to the home page
         end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+     before_action :authenticate_user! #make sure the user is logged in
     def show
         @user = User.find(params[:id])
     end
